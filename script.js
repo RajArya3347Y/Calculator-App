0167d6b7dc1a7457983dcc1f7699c22381822b5b
RajArya3347Y <raajaaryandewangan@gmail.com>
2024-12-18 16:56:27 +0530

    Optimising The App

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ calcButtons.forEach((calcButton) => {
 			lastText = screen.innerText[screen.innerText.length() - 1]
 		}
 		else if (calcButton.innerText === "=") {
-			screen.innerText = eval(screen.innerText).toPrecision(13);
+			screen.innerText = eval(screen.innerText).toPrecision(10);
 		}
 		else if (lastText === "+" || lastText === "-" || lastText === "/" || lastText === "*") {
 			if (calcButton.innerText === "+" || calcButton.innerText === "/") {
